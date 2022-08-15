e9f61f8c138350a5fa209e764d347b0a5939eb6a
Jibran Latif <jibranlatif@com>
2022-08-16 00:08:01 +0500

    dfd

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World! of Winners";
+    private final String message = "Hello World! of Winners New";
 
     public App() {}
 
